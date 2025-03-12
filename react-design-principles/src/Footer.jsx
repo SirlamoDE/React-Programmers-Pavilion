cdba9551c694145b8f66b19ad8d2fd686b1b8ab5
@SirlamoDE <ollysurfaprod@gmail.com>
2025-03-12 09:46:36 +0000

    Updated the footer section

--- react-design-principles/src/Footer.jsx
+++ react-design-principles/src/Footer.jsx
@@ -5,15 +5,14 @@ const Footer = () => {
     <footer>
         <p>Copyright &copy; 2025. All rights reserved.</p>
         <p>Powered by React and React Router</p>
-        <p>Designed by Me</p>
+        <p>Designed by Mesh-React</p>
 
-        {/* Add your social media icons here */}
+       
         <a href="https://github.com/yourusername" target="_blank"><i className="fab fa-github"></i></a>
-        <a href="https://www.linkedin.com/in/yourusername" target="_blank"><i className="fab fa-linkedin"></i></a>
         <a href="https://twitter.com/yourusername" target="_blank"><i className="fab fa-twitter"></i></a>
         <a href="https://www.instagram.com/yourusername" target="_blank"><i className="fab fa-instagram"></i></a>
 
-        {/* Add your email address here */}
+     
       
     </footer>
   )
